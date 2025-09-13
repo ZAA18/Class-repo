d0f34decd3bcf131e6ceb1b5b45a8eab4c5e04d1
ZAA18 <Zubuhle01@gmail.com>
2025-09-13 15:10:06 +0200

    Map Added (NEEDS CREATIVITY)
    THE MAP ONLY SHOWS THE TOP FLOOR
    
    LIKE ITS COMPLICATED ( JUST NEEDS A LITTLE BIT OF THOUGHT)

--- FPS/Assets/Scripts/DoorInteraction.cs
+++ FPS/Assets/Scripts/DoorInteraction.cs
@@ -36,4 +36,4 @@ public class DoorInteraction : MonoBehaviour
 
         transform.rotation = targetRotation;
     }
-}
+} 
---
+++ FPS/Assets/Scripts/Follow Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float OffsetX;
+    [SerializeField] private float OffsetZ;
+    [SerializeField] private float LerpSpeed;
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void LastUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+            new Vector3(target.position.x + OffsetX, transform.position.y, target.position.z + OffsetZ), LerpSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
