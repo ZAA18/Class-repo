b76a8a7077c3047d19a7aa6166ee072a52e7c79d
ZAA18 <Zubuhle01@gmail.com>
2025-09-22 04:23:25 +0200

    Winning condition added
    -I have added the winning condition-
    -I have set the condition to stop the game after you the player has pickup the money bag.
    
    -what's next-
    .... Adding the objectives.....
    .add smoke when the player dies or splatter....]

--- FPS/Assets/Scripts/Winning Item.cs
+++ FPS/Assets/Scripts/Winning Item.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
 
 public class WinningItem : MonoBehaviour
 {
-    public string WinSceneName = "WinScene";
+    [Header("Win Settings")]
+    public string WinSceneName = "WinScene"; // load scene
+    public int pointsOnPickup = 100;
+    public bool destroyOnpickup = true;
 
     public void collect()
     {
         Debug.Log("You got the money! You Win!");
-       // SceneManager.LoadScene(WinSceneName);
+
+        if (destroyOnpickup)
+        { Destroy(gameObject); }
+
+        Time.timeScale = 0;
     }
 }
