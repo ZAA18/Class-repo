01b8cc4cf83c1bfa39fa586efd8d82a14b2b45bc
ZAA18 <Zubuhle01@gmail.com>
2025-10-12 15:54:40 +0200

    Update pickUp mechanics.
    - When you picked up an object the object changed shape when its held in the parent position ( HoldPoint), so now when you pick up an object it doesn't change shape. Going to the throw mechanic. The reason I am fixing these is because in the future we will need to carry staff ( Like money, notes, and others....)

--- FPS/Assets/Scripts/PickUpObject.cs
+++ FPS/Assets/Scripts/PickUpObject.cs
@@ -4,6 +4,7 @@ public class PickUpObject : MonoBehaviour
 {
 
     private Rigidbody rb;
+    private Vector3 originalWorldScale;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -20,18 +21,22 @@ public class PickUpObject : MonoBehaviour
         {
             rb.linearVelocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
-
             rb.useGravity = false;
             rb.isKinematic = true; // prevent the physics from fighting
         }
 
-
-        transform.SetParent(holdPoint, worldPositionStays: true);
+        originalWorldScale = transform.lossyScale;
+        transform.SetParent(holdPoint);
         transform.localPosition = Vector3.zero; //Snap to hold point
         transform.localRotation = Quaternion.identity;
 
         // Make sure the holdPoint scale is (1 - 1 -1 ) in the inspector to avoid warping.
-        transform.localScale = Vector3.one; // prevents warping
+        // transform.localScale = Vector3.one; // prevents warping
+
+        //Restore the oroginal world Scale
+
+        transform.localScale = Vector3.one;
+        transform.localScale = new Vector3(originalWorldScale.x / transform.lossyScale.x, originalWorldScale.y / transform.lossyScale.y, originalWorldScale.z / transform.lossyScale.z);
 
     }
 
@@ -43,16 +48,17 @@ public class PickUpObject : MonoBehaviour
             rb.isKinematic = false;
         }
 
-        transform.SetParent(null, true);
-        transform.localScale = Vector3.one;
+        originalWorldScale = transform.lossyScale;
+        transform.SetParent(null);
+        transform.localScale = originalWorldScale;
     }
 
-    public void MoveToHoldPoint(Vector3 targetPosition)
+ /*   public void MoveToHoldPoint(Vector3 targetPosition)
     {
         if (rb != null)
 
             rb.MovePosition(targetPosition);
         else
             transform.position = targetPosition;
-    }
+    }*/
 }
