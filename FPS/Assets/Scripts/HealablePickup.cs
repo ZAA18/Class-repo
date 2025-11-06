e75ee60558056f22178874bccde4f397ded1de76
ZAA18 <Zubuhle01@gmail.com>
2025-11-07 00:26:15 +0200

    Added Healable Script
    -Added Healable Script....
    
    what's next?
    
    Placing Healable in the scene

---
+++ FPS/Assets/Scripts/HealablePickup.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Collider))]
+public class HealablePickup : MonoBehaviour
+{
+    public enum HealMode { AddAmount, FullRestore }
+
+    [Header("Pickup Settings")]
+    public HealMode healMode = HealMode.AddAmount;
+    public float healAmount = 30f;
+    public string playerTag = "Player";
+    public bool destroyOnPickup = true;
+
+    [Header("Sound for PickUp")]
+    public UnityEvent onPicked;
+
+    private void Reset()
+    {
+        var col = GetComponent<Collider>();
+        if (col == null)
+            col = gameObject.AddComponent<BoxCollider>();
+        col.isTrigger = true;
+    }
+
+    private void Awake()
+    {
+        var col = GetComponent<Collider>();
+        if (col != null) col.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(playerTag))
+            return;
+
+        FPCONTROLLER fp = other.GetComponent<FPCONTROLLER>();
+
+        if (fp == null) 
+            
+        fp = other.GetComponentInParent<FPCONTROLLER>();
+
+        if (fp == null)
+        {
+            Debug.LogWarning($"HealablePickUp: no FPCONTROLLER Found on Object {other.name}");
+            return;
+        }
+
+        ApplyPickupTo(fp);
+
+    }
+
+    private void ApplyPickupTo(FPCONTROLLER fp)
+    {
+        if (healMode == HealMode.AddAmount)
+            fp.Heal(healAmount);
+        else
+            fp.RestoreToFull();
+
+        onPicked?.Invoke();
+
+        if (destroyOnPickup)
+            Destroy(gameObject);
+        else gameObject.SetActive(false);
+    }
+}
